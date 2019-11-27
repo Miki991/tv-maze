8c346deaebd02ce63bbb11951a94dd75b8492e85
miljanabogdanovic1991 <bogdanovic.miljana91@gmail.com>
2019-11-27 21:20:14 +0100

    Updates

---
+++ precache-manifest.009e25808e6190df9baf18b33c99a709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763e5b2ddb50ef622cb7d400edd64008",
+    "url": "/tv-maze/index.html"
+  },
+  {
+    "revision": "ef26cf4ee2e400410ba9",
+    "url": "/tv-maze/static/css/2.a881067f.chunk.css"
+  },
+  {
+    "revision": "52f1773d1b5eb929081e",
+    "url": "/tv-maze/static/css/main.fe502293.chunk.css"
+  },
+  {
+    "revision": "ef26cf4ee2e400410ba9",
+    "url": "/tv-maze/static/js/2.f9b4cc87.chunk.js"
+  },
+  {
+    "revision": "52f1773d1b5eb929081e",
+    "url": "/tv-maze/static/js/main.75921900.chunk.js"
+  },
+  {
+    "revision": "c9a079843a5c710b6b2a",
+    "url": "/tv-maze/static/js/runtime-main.78574454.js"
+  }
+]);
